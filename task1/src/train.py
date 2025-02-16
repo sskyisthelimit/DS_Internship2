5a67cf7c216191c66cacae56f0acef01baa03b4b
Yalanskyi Roman <keito.eternal@gmail.com>
2025-02-16 19:55:25 +0200

    fix: save, load functions

--- task1/src/train.py
+++ task1/src/train.py
@@ -18,19 +18,19 @@ def train_models(datapath, batch_size, device, weights_save_dir):
     print("Starting to train CNN")
     cnn_classifier.train(train_loader, epochs=10)
     cnn_weights_path = os.path.join(weights_save_dir, "cnn_weights.pth")
-    cnn_classifier.save(cnn_weights_path)
+    cnn_classifier.model.save(cnn_weights_path)
     
     fcnn_classifier = MnistClassifier("nn", device=device)
     print("Starting to train FCNN")
     fcnn_classifier.train(train_loader, epochs=10)
     fcnn_weights_path = os.path.join(weights_save_dir, "fcnn_weights.pth")
-    fcnn_classifier.save(fcnn_weights_path)
+    fcnn_classifier.model.save(fcnn_weights_path)
 
     rf_classifier = MnistClassifier("rf", device=device)
     print("Starting to train RF")
     rf_classifier.train(train_loader)
     rf_weights_path = os.path.join(weights_save_dir, "rf_weights.gz")
-    rf_classifier.save(rf_weights_path)
+    rf_classifier.model.save(rf_weights_path)
 
 
 if __name__ == '__main__':
